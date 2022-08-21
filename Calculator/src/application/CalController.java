a42dff9114232e8d742b7507ebb992408e5e293c
20191570 <rlawogns0513@naver.com>
2022-08-21 14:31:45 +0900

    modify UI

--- Calculator/src/application/CalController.java
+++ Calculator/src/application/CalController.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -24,7 +25,6 @@ public class CalController implements Initializable{
 	@FXML private Button eight;
 	@FXML private Button nine;
 	@FXML private Button zero;
-	@FXML private Button save;
 	@FXML private Button del;
 	@FXML private Button mul;
 	@FXML private Button div;
@@ -33,14 +33,18 @@ public class CalController implements Initializable{
 	@FXML private Button answer;
 	@FXML private Button clear;
 	@FXML private Button dot;
-
+	@FXML private Button rring;
+	@FXML private Button lring;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
-		//textArea와 textField에 바로 작성하지 못하게 하기
-		textArea.setDisable(true);
-		textField.setDisable(true);
+		//textArea와 textField에 작성된 것을 화면에서 수정하지 못하도록
+		textArea.setEditable(false);
+		textField.setEditable(false);
+		
+		//답 나오는 부분 오른쪽 정렬
+		textField.setAlignment(Pos.CENTER_RIGHT);
 		
 		//Font font = new Font(null, 0);
 		//textArea.setFont(font);
@@ -59,21 +63,27 @@ public class CalController implements Initializable{
 		eight.setOnAction(e -> textArea.appendText("8"));
 		nine.setOnAction(e -> textArea.appendText("9"));
 		dot.setOnAction(e -> textArea.appendText("."));
+		
+		//split 해야함.
 		plus.setOnAction(e -> textArea.appendText("+"));
 		minus.setOnAction(e -> textArea.appendText("-"));
-		mul.setOnAction(e -> textArea.appendText("*"));
+		mul.setOnAction(e -> textArea.appendText("×"));
 		div.setOnAction(e -> textArea.appendText("/"));
-		save.setOnAction(e -> {
-			
-		});
+		lring.setOnAction(e -> textArea.appendText("("));
+		rring.setOnAction(e -> textArea.appendText(")"));
+		
+		
 		del.setOnAction(e -> {
 			
 		});
 		clear.setOnAction(e -> {
 			
 		});
+		//'='을 작성하면 계산결과가 출력되도록 해야 하는데...
 		answer.setOnAction(e -> {
-			
+			String str = textArea.getText();
+			textField.setText(str);
+			textArea.setText(""); //숫자 입력하는 곳 초기화	
 		});
 
 		
